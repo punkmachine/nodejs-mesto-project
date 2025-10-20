f8c7c2e0056e1150b2ca5f2b1603b52c0aa549a6
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2025-10-20 10:46:35 +0500

    feat: поправил deleteCardValidation с правильным параметром

--- src/helpers/validations/cardValidations.ts
+++ src/helpers/validations/cardValidations.ts
@@ -16,6 +16,6 @@ export const cardIdValidation = celebrate({
 
 export const deleteCardValidation = celebrate({
   params: Joi.object().keys({
-    id: Joi.string().required().hex().length(24),
+    cardId: Joi.string().required().hex().length(24),
   }),
 });
